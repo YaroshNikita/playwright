7f9a4819d293c4a199d9c45db35e2bc8a61d8bb9
yaroshng <yarosh.nik.00@gmail.com>
2024-03-16 16:00:39 +0100

    set project on macOs

--- basicScript.js
+++ basicScript.js
@@ -1,10 +1,11 @@
 const{chromium} = require('playwright');
 
 (async() => {
-    const browser = await chromium.launch({headless:false})
+    const browser = await chromium.launch({headless: false, slowMo: 300})
     const context = await browser.newContext()
     const page = await context.newPage()
     await page.goto('https://app.mars.magmamath.com')
+    
     //const signIn = await page.$('.form-button') by class
     //const signIn = await page.$('button') by css-selector
     //const signIn = await page.$('//button[@class = "form-button signin"]') //by xpath
@@ -14,11 +15,14 @@ const{chromium} = require('playwright');
        await input[0].fill('vovochka_1999')
        await input[1].fill('123456')
 
+     //await page.fill('input', 'vovochka_1999')  
+
+
     const passwordVisibilityIcon = await page.$('//div[@class="show-password-icon"]')
-    passwordVisibilityIcon.click()
+    await passwordVisibilityIcon.click()
 
     const signIn = await page.$('button >> "Log in"') //by xpath
-    signIn.click()
+    await signIn.click()
     await page.screenshot({path:'todo.png'})
     await browser.close()
 }) ()
---
+++ webElements.js
@@ -0,0 +1,30 @@
+const{chromium} = require('playwright');
+
+(async() => {
+    const browser = await chromium.launch({headless:false, slowMo: 100})
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto('https://app.mars.magmamath.com')
+    
+
+
+
+
+
+    // //const signIn = await page.$('.form-button') by class
+    // //const signIn = await page.$('button') by css-selector
+    // //const signIn = await page.$('//button[@class = "form-button signin"]') //by xpath
+    // //const signIn = await page.$('text="Log in"') //by text
+      
+    // const input = await page.$$('input')
+    //    await input[0].fill('vovochka_1999')
+    //    await input[1].fill('123456')
+
+    // const passwordVisibilityIcon = await page.$('//div[@class="show-password-icon"]')
+    // await passwordVisibilityIcon.click()
+
+    // const signIn = await page.$('button >> "Log in"') //by xpath
+    // await signIn.click()
+    // await page.screenshot({path:'todo.png'})
+    // await browser.close()
+}) ()
