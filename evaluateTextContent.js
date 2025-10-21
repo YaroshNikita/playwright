879af9e212a368c9e9e79850fec02bbac44383f3
YaroshNikita <yarosh.nik.00@gmail.com>
2025-10-21 18:50:03 +0200

    added expect

--- evaluateTextContent.js
+++ evaluateTextContent.js
@@ -1,7 +1,8 @@
 const{chromium} = require('playwright');
+const expect = require('expect').expect;
 
 (async() => {
-    const browser = await chromium.launch({headless: true, slowMo: 300})
+    const browser = await chromium.launch({headless: false, slowMo: 300})
     const context = await browser.newContext()
 
     const page = await context.newPage()
@@ -11,15 +12,22 @@ const{chromium} = require('playwright');
 const sso = await page.$$('._Typography_1ki3q_1._HeadingNine_1ki3q_50._Text_nrl0w_13._BuenosAires_1ki3q_100')
 const skolon = await sso[3].evaluate(el => el.textContent)
 console.log(skolon);
-
+expect(skolon).toBe('Skolon')
+   
 //#2
 const google = await page.$$eval('._Typography_1ki3q_1._HeadingNine_1ki3q_50._Text_nrl0w_13._BuenosAires_1ki3q_100', el => el[0].textContent)
 console.log(google);
+expect(google).toBe('Google')
 
 //#3
 const ssoList = await page.$$eval('._Typography_1ki3q_1._HeadingNine_1ki3q_50._Text_nrl0w_13._BuenosAires_1ki3q_100', els => els.map(el => el.textContent))
 console.log(ssoList);
+expect(ssoList).toStrictEqual(["Google", "Microsoft", "Skolfederation", "Skolon", "Magma QR-kod"])
 
+//#3
+const ssoCount = await page.$$eval('._Typography_1ki3q_1._HeadingNine_1ki3q_50._Text_nrl0w_13._BuenosAires_1ki3q_100', els => els.length)
+console.log(ssoCount);
+expect(ssoCount).toEqual(5)
 //els => els[0].textContent	Возвращает строку
 //els => els.map(el => el.textContent)	Возвращает массив
 
